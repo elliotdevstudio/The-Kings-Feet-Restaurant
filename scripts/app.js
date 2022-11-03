a6b759f8fb085595e21195e012eae25ed0326b06
ElliotHarrison <ellioth1029@gmail.com>
2022-11-03 12:19:20 -0700

    adds more templates and further sytles added to homepage

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+
+/* sample of the animation running on scroll, for product image/description
+    transition: opacity 800ms cubic-bezier(.4,0,.2,1),transform 800ms cubic-bezier(.4,0,.2,1);
+    transition-property: opacity, transform;
+    transition-duration: 800ms, 800ms;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1), cubic-bezier(0.4, 0, 0.2, 1);
+    transition-delay: 0s, 0s;
+
+
+
+    data-image-dimensions="1202x1202" data-image-focal-point="0.5,0.5" alt="Screen Shot 2021-06-05 at 4.41.00 PM.png" data-load="false" style="height: 314px; width: 100%; object-position: 50% 50%; object-fit: cover;" data-parent-ratio="1.0" class="" data-image-resolution="750w" src=""></img>
+*/
+
+    /*lightbox styling for focused image in gallery */
