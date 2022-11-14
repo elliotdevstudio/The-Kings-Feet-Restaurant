f9c22f0a0db82e35d6c8833d443b4f30cb50f932
ElliotHarrison <ellioth1029@gmail.com>
2022-11-14 15:30:00 -0800

    homepage animations and styles completed

--- scripts/app.js
+++ scripts/app.js
@@ -11,4 +11,67 @@
     data-image-dimensions="1202x1202" data-image-focal-point="0.5,0.5" alt="Screen Shot 2021-06-05 at 4.41.00 PM.png" data-load="false" style="height: 314px; width: 100%; object-position: 50% 50%; object-fit: cover;" data-parent-ratio="1.0" class="" data-image-resolution="750w" src=""></img>
 */
 
-    /*lightbox styling for focused image in gallery */
+    /*lightbox styling for focused image in gallery 
+
+const navMenuMobile = document.getElementById("navbar-burger");
+const navModal = document.getElementById("navModal");
+function firstFunction(){
+    navModal.style.display ="flex";
+}
+function secondFunction() {
+    
+}
+function navMenuModal (e) {
+    e.preventDefault();
+    firstFunction();
+    secondFunction();
+}
+
+navMenuModal.onclick = navMenuModal;
+*/
+
+const navbarBurger = document.getElementById('navbar-burger');
+const topBun = document.getElementById('top-bun');
+const bottomBun = document.getElementById('bottom-bun');
+
+navbarBurger.addEventListener('click', () => {
+    topBun.classList.toggle('active');
+    bottomBun.classList.toggle('active');
+});
+
+/* SHOW ON SCROLL */
+
+var scroll = window.requestAnimationFrame || 
+    function(callback) { window.setTimeout(callback, 1000/60)};
+
+var elementsToShow = document.querySelectorAll('.show-on-scroll');
+
+function loop() {
+    elementsToShow.forEach( function (element) {
+        if (isElementInViewport(element)) {          
+            element.classList.add('is-visible');
+        }  else  {
+               element.classList.remove('is-visible');
+        }
+    });
+    scroll(loop);
+};
+loop();
+
+function isElementInViewport(el){
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+var rect = el.getBoundingClientRect();   // this is the wrapper we want to check
+    return (
+        (rect.top <= 0 && rect.bottom >= 0)
+        ||
+        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+             rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+        ||
+        (rect.top >= 0 && rect.bottom <= (window.innerHeight || 
+            document.documentElement.clientHeight))
+        );
+        }
+
